1c4b4f1a7eade8018c8c1a0e22026dc4be2d86ff
Tomotada Saigusa <nirot1r@g-second.net>
2018-10-24 15:58:05 +0900

    デザイン作成

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <nuxt />
+</template>
+
+<style>
+* {
+  margin: 0;
+  font-family: "Yu Gothic Medium", "游ゴシック Medium", YuGothic, "メイリオ", sans-serif;
+  transition: all 100ms ease-in;
+}
+
+html, body {
+  margin: 0; padding: 0;
+  background-color: #f2f2f2;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="column">
+        <div class="card">
+          <div class="card-status"><span class="status-circle ok"/>宇都宮線</div>
+        </div>
+        <div class="card">
+          <div class="card-status"><span class="status-circle ok"/>宇都宮線</div>
+        </div>
+      </div>
+      <div class="column">
+        <div class="card">
+          <div class="card-status"><span class="status-circle ng"/>aA</div>
+        </div>
+        <div class="card">
+          <div class="card-status"><span class="status-circle ok"/>宇都宮線</div>
+        </div>
+      </div>
+      <div class="column">
+        <div class="card">
+          <div class="card-status"><span class="status-circle ok"/>宇都宮線</div>
+        </div>
+        <div class="card">
+          <div class="card-status"><span class="status-circle ok"/>宇都宮線</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+.container {
+  margin: 50px 5%;
+}
+.row {
+  font-size: 0;
+}
+
+.column {
+  width: 33.3%;
+  display: inline-block;
+  font-size: 1rem;
+  vertical-align: top;
+}
+
+@media screen and (max-width: 800px) {
+
+  .column {
+    width: 100%;
+    display: block;
+    font-size: 1rem;
+  }
+
+}
+
+.card {
+  margin: 5px;
+  padding: 10px;
+  background-color: #ffffff;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.5);
+  color: #666666;
+  font-size: 2em;
+  font-weight: bold;
+  text-align: center;
+}
+
+.status-circle::before {
+  display: inline-block;
+  position: relative;
+  vertical-align: middle;
+  content: '';
+  width: 20px;
+  height: 20px;
+  margin: 10px;
+  top: -0.1em;
+  border-radius: 50%;
+  background-color: #222222;
+}
+
+.status-circle.ok::before { background-color: #0bc01a; }
+.status-circle.ng::before { background-color: #c00b0b; }
+</style>
